010a1d66245acba404d5f21ddabddd26639074c8
Ana Walton <awalton3@nd.edu>
2019-11-03 11:37:14 -0500

    Added routing for login component

--- storyLine/src/app/app-routing.module.ts
+++ storyLine/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: '**', redirectTo: 'login', pathMatch: 'full' }
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
