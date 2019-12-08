cfea9cc9970cc136d30b33a0c7ade70e4d6a1184
Molly Zachlin <mzachlin@gmail.com>
2019-12-08 18:53:36 -0500

    Fix bookmarking and add like count to stories

--- storyLine/src/app/home/stories/stories.component.ts
+++ storyLine/src/app/home/stories/stories.component.ts
@@ -12,31 +12,19 @@ export class StoriesComponent implements OnInit {
   @Input() story : any;
   bookmarked : number;
   bookmarkObj : any;
+  liked : number;
+  likeObj : any;
 
   constructor(private sql: SQLService) { }
   private subs: Subscription = new Subscription();
 
   ngOnInit() {
     this.getBookmarked()
-  }
-
-  onBookmark() {
-    this.bookmarkObj = {'storyHashID':this.story.storyHashID, 'authorUsername':sessionStorage.getItem('username')}
-    console.log(this.bookmarked);
-    if (this.bookmarked) {
-      this.bookmarked = 0;
-      console.log('unbookmarked');
-      this.subs.add(this.sql.removeBookmark(this.bookmarkObj).subscribe(result => {}));
-    }
-    else {
-      console.log('bookmarked');
-      this.bookmarked = 1;
-      this.subs.add(this.sql.addBookmark(this.bookmarkObj).subscribe(result => {}));
-    }
+    this.getLiked()
   }
 
   getBookmarked() {
-    console.log(this.sql.getBookmarked(this.story.storyHashID));
+    //console.log(this.sql.getBookmarked(this.story.storyHashID));
     this.subs.add(this.sql.getBookmarked(this.story.storyHashID).subscribe(result => {
       if (result['bookmarked']) {
         this.bookmarked = 1
@@ -46,5 +34,53 @@ export class StoriesComponent implements OnInit {
       }
     }));
   }
+
+  onBookmark() {
+    this.bookmarkObj = {'storyHashID':this.story.storyHashID, 'authorUsername':sessionStorage.getItem('username')}
+    //console.log(this.bookmarked);
+    if (this.bookmarked) {
+      this.bookmarked = 0;
+      console.log('just unbookmarked');
+      this.subs.add(this.sql.removeBookmark(this.bookmarkObj).subscribe(result => {}));
+    }
+    else {
+      console.log('just bookmarked');
+      this.bookmarked = 1;
+      this.subs.add(this.sql.addBookmark(this.bookmarkObj).subscribe(result => {}));
+    }
+  }
+
+  getLiked() {
+      console.log('in get liked')
+      console.log(this.sql.getLiked(this.story.storyHashID))
+      this.subs.add(this.sql.getLiked(this.story.storyHashID).subscribe(result => {
+          if (result['liked']) {
+              console.log("I am liked!\n")
+              this.liked = 1
+          }
+          else {
+              console.log("I am not liked!\n")
+              this.liked = 0
+          }
+    }));
+   }
+
+
+  onLike() {
+      this.likeObj = {'storyHashID':this.story.storyHashID, 'authorUsername':sessionStorage.getItem('username')}
+      console.log(this.liked);
+      if (this.liked) {
+        this.liked = 0;
+        this.subs.add(this.sql.removeLike(this.likeObj).subscribe(result => {}));
+        this.story['change'] = 0;
+        this.subs.add(this.sql.updateStoriesNumUpVotes(this.story).subscribe(result => {}));
+      }
+      else {
+        this.liked = 1;
+        this.subs.add(this.sql.addLike(this.likeObj).subscribe(result => {}));
+        this.story['change'] = 1;
+        this.subs.add(this.sql.updateStoriesNumUpVotes(this.story).subscribe(result => {}));
+      }
+    }
 
 }
