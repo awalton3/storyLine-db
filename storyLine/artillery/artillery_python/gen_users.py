564548c670d14ce7cf25ed257df79ec4643e45f8
Molly Zachlin <mzachlin@gmail.com>
2021-04-28 21:57:09 -0400

    Unstage ridiculously large users.csv

--- storyLine/artillery/artillery_python/gen_users.py
+++ storyLine/artillery/artillery_python/gen_users.py
@@ -4,5 +4,5 @@ import csv
 
 with open('../artillery_csv/users.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for i in range(50000):
+    for i in range(60000000):  # Make this smaller!
         writer.writerow([f'test{i}@test', f'test{i}', f'test{i}'])
